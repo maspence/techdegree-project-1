b16e8de6c632bdd16a77d25163040aeb9bfdd6c1
Andy Spence <39201011+maspence@users.noreply.github.com>
2018-05-23 19:01:25 -0400

    Updated quotes and added array elements

--- js/script.js
+++ js/script.js
@@ -4,24 +4,29 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 //array of quotes 
 var quotes = [
-	{quote:'I refuse to join any club that would have me as a member.', 
+	{quote:"I don’t want to belong to any club that would accept me as one of its members.", 
 		person: 'Groucho Marx',
-			citation:
+			citation: 'Dunkirk Evening Observer'
+					year: '1949'
 		}	
 	{quote:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
 	 	person: 'J.R.R Tolkien',
-	  		citation: 'The Lord of the Rings'
+	  		citation: 'The Lord of the Rings',
+	  				year: '1954'
 	  	}
 	{quote:"Behind every great man is a woman rolling her eyes.",
 	 	person: 'Jim Carrey',
 	  		citation:
+	  				year:
 	  	}
 	{quote: 'History will be kind to me for I intend to write it',
 		 person: 'Winston Churhill' ,
 	  		citation:
+	  				year:
 	  	}
 	{quote: 'To infinity and beyond!' ,
 	 	person: 'Buzz Lightyear' ,
-	  		citation: 'Toy Story'
+	  		citation: 'Toy Story',
+	  				year:
 	  	}
 ];
