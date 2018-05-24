80d2aa3dbccc2ca72ef2552ad120210fb5485053
Andy Spence <39201011+maspence@users.noreply.github.com>
2018-05-23 20:16:01 -0400

    Updated formatting and added functions

--- js/script.js
+++ js/script.js
@@ -6,27 +6,34 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 var quotes = [
 	{quote:"I don’t want to belong to any club that would accept me as one of its members.", 
 		person: 'Groucho Marx',
-			citation: 'Dunkirk Evening Observer'
-					year: '1949'
-		}	
+			citation: 'Dunkirk Evening Observer',
+				year: '1949'
+		},	
 	{quote:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
 	 	person: 'J.R.R Tolkien',
 	  		citation: 'The Lord of the Rings',
-	  				year: '1954'
-	  	}
+	  			year: '1954'
+	  	},
 	{quote:"Behind every great man is a woman rolling her eyes.",
 	 	person: 'Jim Carrey',
-	  		citation:
-	  				year:
-	  	}
+	  	},
 	{quote: 'History will be kind to me for I intend to write it',
 		 person: 'Winston Churhill' ,
-	  		citation:
-	  				year:
-	  	}
-	{quote: 'To infinity and beyond!' ,
+	  	},
+	{quote: 'To infinity...and beyond!' ,
 	 	person: 'Buzz Lightyear' ,
 	  		citation: 'Toy Story',
-	  				year:
+	  			year: '1995'
 	  	}
 ];
+
+
+
+//function to retrieve quote from array
+function getRandomQuote() {
+
+}
+ //print random quote to page
+function printQuote() {
+
+}
