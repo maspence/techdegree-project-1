12ade8aa19381829c495d122a9fbdb598a895027
Andy Spence <39201011+maspence@users.noreply.github.com>
2018-05-28 17:08:41 -0400

    Added randomQuote function and printQuote function

--- js/script.js
+++ js/script.js
@@ -5,35 +5,48 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 //array of quotes 
 var quotes = [
 	{quote: "I don’t want to belong to any club that would accept me as one of its members.", 
-		person: 'Groucho Marx',
+		source: 'Groucho Marx',
 			citation: 'Dunkirk Evening Observer',
 				year: '1949'
 		},	
 	{quote: "It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
-	 	person: 'J.R.R Tolkien',
+	 	source: 'J.R.R Tolkien',
 	  		citation: 'The Lord of the Rings',
 	  			year: '1954'
 	  	},
 	{quote: "Behind every great man is a woman rolling her eyes.",
-	 	person: 'Jim Carrey',
+	 	source: 'Jim Carrey'
 	  	},
 	{quote: 'History will be kind to me for I intend to write it',
-		 person: 'Winston Churhill' ,
+		 source: 'Winston Churhill'	 
 	  	},
 	{quote: 'To infinity...and beyond!' ,
-	 	person: 'Buzz Lightyear' ,
+	 	source: 'Buzz Lightyear' ,
 	  		citation: 'Toy Story',
 	  			year: '1995'
 	  	}
 ];
 
-
+var htmlMessageOutput = '';
 
 //function to retrieve quote from array
-function getRandomQuote() {
-
+function getRandomQuote(quote) {
+	var randomNumber = Math.floor(Math.random() * quotes.length);
+	var randomQuote = quotes[randomNumber];
+	// console.log(randomQuote.quote);
+	return randomQuote;
 }
  //print random quote to page
 function printQuote() {
-
+ quoteInformation = getRandomQuote()
+	htmlMessageOutput = "<p class='quote'>" + quoteInformation.quote + "</p>";
+	htmlMessageOutput += "<p class='source'>" + quoteInformation.source + "</p>";
+	if ( quoteInformation.citation !== undefined ) {
+		htmlMessageOutput += "<span class='citation'>" + quoteInformation.citation + '</span>';
+	} 
+	if ( quoteInformation.year !== undefined ) {
+		htmlMessageOutput += '<span class="year">' + quoteInformation.year + '</span>';
+	}
 }
+
+document.getElementById('quote-box').innerHTML = htmlMessageOutput;
