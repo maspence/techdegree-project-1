7599a99258b7caa05a534657e41ef38d06902194
Andy Spence <39201011+maspence@users.noreply.github.com>
2018-06-02 16:04:59 -0400

    Updated Quotes and refactoring

--- js/script.js
+++ js/script.js
@@ -13,32 +13,37 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 //array of quotes 
 var quotes = [
-	{quote: "I don’t want to belong to any club that would accept me as one of its members.", 
-		source: 'Groucho Marx',
-			citation: 'Dunkirk Evening Observer',
-				year: '1949',
-					tag: 'Funny'
-		},	
-	{quote: "It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
-	 	source: 'J.R.R Tolkien',
-	  		citation: 'The Lord of the Rings',
-	  			year: '1954',
-	  				tag: 'Book, Inspirational'
-	  	},
-	{quote: "Behind every great man is a woman rolling her eyes.",
-	 	source: 'Jim Carrey',
-	 		tag: 'Funny'
-	  	},
-	{quote: 'History will be kind to me for I intend to write it',
-		 source: 'Winston Churhill',
-		 	tag: 'Arrogant, Insightful'	 
-	  	},
-	{quote: 'To infinity...and beyond!' ,
-	 	source: 'Buzz Lightyear' ,
-	  		citation: 'Toy Story',
-	  			year: '1995',
-	  				tag: 'Inspirational, Movie'
-	  	}
+	{
+	quote: "I don’t want to belong to any club that would accept me as one of its members.", 
+	source: 'Groucho Marx',
+	citation: 'Dunkirk Evening Observer',
+	year: '1949',
+	tag: 'Funny'
+},	
+	{
+	quote: "It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
+	source: 'J.R.R Tolkien',
+	citation: 'The Lord of the Rings',
+	year: '1954',
+	tag: 'Book, Inspirational'
+},
+	{
+	quote: "Behind every great man is a woman rolling her eyes.",
+	source: 'Jim Carrey',
+	tag: 'Funny'
+},
+	{
+	quote: 'History will be kind to me for I intend to write it',
+	source: 'Winston Churhill',
+	tag: 'Arrogant, Insightful'	 
+},
+	{
+	quote: 'To infinity...and beyond!' ,
+	source: 'Buzz Lightyear' ,
+	citation: 'Toy Story',
+	year: '1995',
+	tag: 'Inspirational, Movie'
+}
 ];
 
 var htmlMessageOutput = '';
@@ -72,7 +77,7 @@ function getRandomQuote(quote) {
 }
  //print random quote to page
 function printQuote() {
- quoteInformation = getRandomQuote()
+ let quoteInformation = getRandomQuote()
 	htmlMessageOutput = `<p class='quote'>${quoteInformation.quote} </p>`;
 	htmlMessageOutput += `<p class='source'>${quoteInformation.source}</p>`;
 
